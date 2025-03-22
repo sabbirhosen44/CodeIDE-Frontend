75fb2d303913a08e4dff675101eb6311c094c8e0
Jawwad926 <mdsabbirhosen926@gmail.com>
2025-03-22 22:59:14 +0600

    feat: initialize MERN project structure with basic setup
    - Add initial config files (.gitignore, README.md, .env.example)
    
    - Set up package.json with Express and Node.js dependencies
    
    - Create client/ for React frontend and server/ for backend

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button className="bg-teal-500">Welcome to Code IDE!</Button>
+    </div>
+  );
+}
+
+export default App;
