a164acf1fb75fd046e40c6577cf709b552ac4aad
Jawwad926 <mdsabbirhosen926@gmail.com>
2025-04-04 03:42:32 +0600

    feat: simplify ModeToggle to single-button theme switch
    Modified the ModeToggle component to:
    - Remove dropdown menu functionality
    - Add direct theme toggling on button click using current theme state
    - Preserve sun/moon icon transitions for visual feedback

--- client/src/components/ModeToggle.tsx
+++ client/src/components/ModeToggle.tsx
@@ -10,29 +10,20 @@ import {
 } from "./ui/dropdown-menu";
 
 const ModeToggle = () => {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
           variant="outline"
           size="icon"
-          className="relative flex items-center justify-center  "
+          className="relative flex items-center justify-center"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
           <FiSun className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <FiMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuGroup>
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
